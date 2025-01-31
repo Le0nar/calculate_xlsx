6dbc59323f7aa41f3ea3230ca3e06c2177beba71
Le0nar <yaroslavtrefilov5@gmail.com>
2025-01-31 17:17:16 +0400

    feat: change cretead excel file

--- cmd/create_test_file/main.go
+++ cmd/create_test_file/main.go
@@ -15,9 +15,21 @@ func main() {
 	sheet := "Sheet1" // Название листа, можно выбрать любое
 
 	// Заполняем 100000 строк значением 1 в столбце A
-	for i := 1; i <= 100000; i++ {
-		cell := "A" + strconv.Itoa(i)
-		err := f.SetCellValue(sheet, cell, 1)
+	for i := 1; i <= 500000; i++ {
+		cellA := "A" + strconv.Itoa(i)
+		err := f.SetCellValue(sheet, cellA, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cellB := "B" + strconv.Itoa(i)
+		err = f.SetCellValue(sheet, cellB, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cellC := "C" + strconv.Itoa(i)
+		err = f.SetCellValue(sheet, cellC, 1)
 		if err != nil {
 			log.Fatal(err)
 		}
